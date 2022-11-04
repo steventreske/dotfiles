5408f4553e0ac59be8250d6c6389d4e8641df71a
steventreske <steven.treske@hotmail.com>
2022-11-04 21:35:28 +0100

    install and use zsh

--- install.sh
+++ install.sh
@@ -5,9 +5,14 @@ log_file=~/install_progress_log.txt
 
 sudo apt -y install curl
 if type -p vim > /dev/null; then
-    echo "stow Installed" >> $log_file
+    echo "stow Installed" >> $log_file else echo "stow FAILED TO INSTALL!!!" >> $log_file
+fi
+
+sudo apt-get -y install zsh
+if type -p vim > /dev/null; then
+    echo "zsh Installed" >> $log_file
 else
-    echo "stow FAILED TO INSTALL!!!" >> $log_file
+    echo "zsh FAILED TO INSTALL!!!" >> $log_file
 fi
 
 sudo apt-get -y install stow
@@ -24,6 +29,8 @@ else
     echo "NVim FAILED TO INSTALL!!!" >> $log_file
 fi
 
+# make zsh the default shell
+chsh -s /usr/bin/zsh
 
 # install plug manager for nvim
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
